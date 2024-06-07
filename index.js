ad81a000bb6c5e67071968fa6ef1405538bb1586
StephenJP14 <stephen.j.pratama@gmail.com>
2024-06-08 02:25:13 +0700

    change the host

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
